a7a7bdb75c738c67edd9bdc5efbc97785d653c8b
Philippe Coval <philippe.coval@silabs.com>
2025-09-26 12:47:11 +0200

    fix(legal): Workaround reuse tool
    I noticed while using https://reuse.software/
    this false positive error:
    
        reuse.extract - ERROR - Could not parse 'MIT\n','
        reuse.extract - ERROR - \
          '.../FreeRTOS-Kernel/.github/scripts/kernel_checker.py' \
          holds an SPDX expression that cannot be parsed, skipping the file
    
    Origin: https://github.com/FreeRTOS/FreeRTOS-Kernel/pull/1322
    Relate-to: https://github.com/FreeRTOS/FreeRTOS-Kernel/issues/1321
    Relate-to: https://reuse.software/faq/#exclude-lines
    Signed-off-by: Philippe Coval <philippe.coval@silabs.com>

--- .github/scripts/kernel_checker.py
+++ .github/scripts/kernel_checker.py
@@ -125,7 +125,9 @@ KERNEL_HEADER = [
     ' * FreeRTOS Kernel <DEVELOPMENT BRANCH>\n',
     ' * Copyright (C) 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.\n',
     ' *\n',
+# REUSE-IgnoreStart
     ' * SPDX-License-Identifier: MIT\n',
+# REUSE-IgnoreEnd
     ' *\n',
     ' * Permission is hereby granted, free of charge, to any person obtaining a copy of\n',
     ' * this software and associated documentation files (the "Software"), to deal in\n',
